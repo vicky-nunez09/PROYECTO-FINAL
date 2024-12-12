da90092e778c779b08a19b613cdbf08ce3f63c8b
vicky-nunez09 <victorianunez625@gmail.com>
2024-12-12 17:52:18 -0300

    AHORA SÍ código nuevo con txt

--- main.py
+++ main.py
@@ -1,73 +1,87 @@
-import json
-    
-def lector_datos():
-    lista_datos=[]
-    with open("libros.json", "r") as lector:
-        lista_datos=json.load(lector)
-    return lista_datos
+import datetime
 
-def mostrar_libro():
-    datos=lector_datos()
-    print(datos)
+def mostrar_libros():
+    file=open(r"libros.txt","r",encoding="utf-8")
+
+    contenido=file.readlines()
+    listaLibros=[]
+
+    for libro in contenido:
+        contLibro=libro.split(",")
+        dicc={}
+        dicc["Título"]=contLibro[0]
+        dicc["Autor"]=contLibro[1]
+        dicc["Publicación"]=contLibro[2]
+        dicc["Estrellas"]=contLibro[3]
+        dicc["Personaje favorito"]=contLibro[4]
+        dicc["Comentario"]=contLibro[5]
+
+    listaLibros.append(dicc)
+
+    for i in range(len(contenido)):
+        print(contenido[i])
+
+    file.close()
 
 def agregar_libro():
-    try:
-        with open("libros.json", "r") as archivo:
-            libros = json.load(archivo)
-    except (FileNotFoundError, json.JSONDecodeError):
-        libros=[]        
+
+    archivo=open(r"libros.txt","a",encoding="utf-8")
     print("Añadir libro: ")
-    ID=max([libro['id'] for libro in archivo], default=0) + 1
+
+    libro=[]
     titulo=input("Ingrese el título del libro: ")
+    libro.append(titulo)
     autor=input("Ingrese el autor del libro: ")
+    libro.append(autor)
+
     while True:
         try:
-            publicacion=input("Ingrese el año de publicación: ")
-            break
+            publicacion=int(input("Ingrese el año de publicación (negativo para a.C): "))
+            if publicacion>datetime.datetime.now().year:
+                print("Número no válido. Inténtelo nuevamente...")
+            else:
+                break
         except ValueError:
             print("Número no válido. Inténtelo nuevamente...")
-   
+
+    str(publicacion)
+    libro.append(publicacion)
+
     while True:
         try:
-             estrellas=int(input("¿Cuántas estrellas le da? (Ingrese un número, por favor): "))
-             break
+            estrellas=int(input("¿Cuántas estrellas le da? (1-5): "))
+            if estrellas>5:
+                print("Número no válido. Inténtelo nuevamente...")
+            elif estrellas==0:
+                print("Número no válido. Inténtelo nuevamente...")
+            elif estrellas<0:
+                print("Número no válido. Inténtelo nuevamente...")
+            else:
+                break
         except ValueError:
              print("Número no válido. Inténtelo nuevamente...")
-    
+    str(estrellas)
+    libro.append(estrellas)
     personaje_fav=input("Ingrese su personaje favorito: ")
-    reseña=input("Ingrese una reseña: ")
+    libro.append(personaje_fav)
+    comentario=input("Ingrese un comentario: ")
+    libro.append(comentario)
+    archivo.write("\n")
 
-    nuevo_libro={
-        "ID" : ID,
-        "Titulo" : titulo,
-        "Autor" : autor,
-        "Publicacion" : publicacion,
-        "Estrellas" : estrellas,
-        "PersonajeFav" : personaje_fav,
-        "Reseña" : reseña
-    }
-    libros.append(nuevo_libro)
-    with open("libros.json","w") as archivo:
-        json.dump(libros,archivo)
+    for i in libro:
+        archivo.write(i)
+        archivo.write(",")
 
-    print("El libro se ha añadido con éxito.")
-    print(f"Libros registrados: \n{libros}")
-
-    
-
-# def modificar_libro():
-#     print("Título: 1.\nAutor: 2\nAño de publicación: 3\nEstrellas: 4\nPersonaje favorito: 5\nReseña: 6")
-#     dato=input("¿Qué dato desea modificar?")
-#     if dato=="1":
+    print(libro)
 
 
-libros=[]
-while True:
-    print("BIENVENIDO AL DIARIO DE LECTURAS.\n1-Mostrar libros.\n2-Agregar libro.\n3-Modificar libro.\n4-Eliminar libro.\n5-Salir del programa.")
-    actividad=input("Qué actividad desea realizar?: ")
-    if actividad=="1":
-        mostrar_libro()
-    elif actividad=="2":
-        agregar_libro()
-    elif actividad=="3":
-        break
+
+# # while True:
+# #     print("BIENVENIDO AL DIARIO DE LECTURAS.\n1-Mostrar libros.\n2-Agregar libro.\n3-Modificar libro.\n4-Eliminar libro.\n5-Salir del programa.")
+# #     actividad=input("Qué actividad desea realizar?: ")
+# #     if actividad=="1":
+# #         mostrar_libros()
+# #     elif actividad=="2":
+# #         agregar_libro()
+# #     elif actividad=="3":
+# #         break
