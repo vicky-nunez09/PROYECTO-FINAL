806e5c827c1c06c4476d9e130a46a12d3c188249
vicky-nunez09 <victorianunez625@gmail.com>
2024-12-13 21:08:55 -0300

    se ejecutan correctamente las funciones de mostrar y agregar

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import datetime
 
 def mostrar_libros():
-    file=open(r"libros.txt","r",encoding="utf-8")
+    archivo=open(r"libros.txt","r",encoding="utf-8")
 
-    contenido=file.readlines()
+    contenido=archivo.readlines()
     listaLibros=[]
-
+    
     for libro in contenido:
         contLibro=libro.split(",")
         dicc={}
-        dicc["Título"]=contLibro[0]
-        dicc["Autor"]=contLibro[1]
-        dicc["Publicación"]=contLibro[2]
-        dicc["Estrellas"]=contLibro[3]
-        dicc["Personaje favorito"]=contLibro[4]
-        dicc["Comentario"]=contLibro[5]
+        dicc["ISBN"]=contLibro[0]
+        dicc["Título"]=contLibro[1]
+        dicc["Autor"]=contLibro[2]
+        dicc["Publicación"]=contLibro[3]
+        dicc["Estrellas"]=contLibro[4]
+        dicc["Personaje favorito"]=contLibro[5]
+        dicc["Comentario"]=contLibro[6]
+        listaLibros.append(dicc)
 
-    listaLibros.append(dicc)
+    for libro in listaLibros:
+        print(libro)
 
-    for i in range(len(contenido)):
-        print(contenido[i])
-
-    file.close()
+    archivo.close()
 
 def agregar_libro():
 
-    archivo=open(r"libros.txt","a",encoding="utf-8")
     print("Añadir libro: ")
-
-    libro=[]
+    
+    libro=[]    
+    archivo=open(r"libros.txt","a",encoding="utf-8")
+    
+    while True:
+        try:
+            ISBN=int(input("Ingrese el ISBN de su libro (escrito en el código de barra): "))
+            break
+        except ValueError:
+            print("Número no válido. Inténtelo nuevamente...")
+    
+    libro.append(str(ISBN))
+    
     titulo=input("Ingrese el título del libro: ")
     libro.append(titulo)
     autor=input("Ingrese el autor del libro: ")
@@ -44,8 +54,7 @@ def agregar_libro():
         except ValueError:
             print("Número no válido. Inténtelo nuevamente...")
 
-    str(publicacion)
-    libro.append(publicacion)
+    libro.append(str(publicacion))
 
     while True:
         try:
@@ -60,28 +69,37 @@ def agregar_libro():
                 break
         except ValueError:
              print("Número no válido. Inténtelo nuevamente...")
-    str(estrellas)
-    libro.append(estrellas)
+    libro.append(str(estrellas))
     personaje_fav=input("Ingrese su personaje favorito: ")
     libro.append(personaje_fav)
     comentario=input("Ingrese un comentario: ")
     libro.append(comentario)
-    archivo.write("\n")
 
     for i in libro:
         archivo.write(i)
         archivo.write(",")
 
+    archivo.write("\n")
+
     print(libro)
+    archivo.close()
 
+def eliminar_libro():
+    while True:
+        try:
+            isbn_eliminar=int(input("Ingrese el ISBN del libro a eliminar: "))
+            break
+        except ValueError:
+            print("Número no válido. Inténtelo nuevamente...")
+    
 
+while True:
+    print("BIENVENIDO AL DIARIO DE LECTURAS.\n1-Mostrar libros.\n2-Agregar libro.\n3-Modificar libro.\n4-Eliminar libro.\n5-Salir del programa.")
+    actividad=input("Qué actividad desea realizar?: ")
+    if actividad=="1":
+        mostrar_libros()
+    elif actividad=="2":
+        agregar_libro()
+    elif actividad=="4":
+        eliminar_libro()
 
-# # while True:
-# #     print("BIENVENIDO AL DIARIO DE LECTURAS.\n1-Mostrar libros.\n2-Agregar libro.\n3-Modificar libro.\n4-Eliminar libro.\n5-Salir del programa.")
-# #     actividad=input("Qué actividad desea realizar?: ")
-# #     if actividad=="1":
-# #         mostrar_libros()
-# #     elif actividad=="2":
-# #         agregar_libro()
-# #     elif actividad=="3":
-# #         break
